e9e6ca5558437b6249fbdfe7c184bf9af787cfa9
Austin Williams <austinq.williams@gmail.com>
2019-02-23 22:50:58 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,8 @@
 # Build your say_hello method here
- def say_hello(name = "Austin")
-  puts "Hello, #{name}!"
-   
-end 
+ def say_hello(name)
+puts "Hello Kent Beck"
+end
+
+ def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
+end
